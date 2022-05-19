5ac386a552d45b631a5241162cafa8a9db565adc
chan97 <papajuju123@naver.com>
2022-05-19 15:51:00 +0900

    commit control

---
+++ dom.js
@@ -0,0 +1,27 @@
+function name(params) {
+    return (
+        <div>
+            <span>like lion</span>
+            <span>front end</span>
+            <span>keep going</span>
+        </div>
+    )
+}
+
+let nodeObject = {
+    tagName: "div",
+    children: [
+        {
+            tagName: "span",
+            textContent: "like lion"
+        },
+        {
+            tagName: "span",
+            textContent: "front end"
+        },
+        {
+            tagName: "span",
+            textContent: "keep going"
+        }
+    ]
+}
